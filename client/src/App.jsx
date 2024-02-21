fc090e903a01dce63f9c2b77bf37c2690dd59f4b
Ibrahim <ibrahim.soft.dev23@gmail.com>
2024-02-21 12:26:37 +0600

    Update
    go

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 
 function App() {
-  const [theme, SetTheme] = useState('light');
+  
+  const current_them = localStorage.getItem('current_them')
+  const [theme, SetTheme] = useState(current_them ? current_them : 'light');
+
+  useEffect(() => {
+    localStorage.setItem('current_them', theme)
+  },[theme])
   return (
     <div className={`container ${theme}`}>
       <Navbar theme={theme} SetTheme={SetTheme} />
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React from 'react';
 import '../Utils/Navbar.css';
 import logowhite from "../assets/logo-white.png";
-import logoblack from "../assets/logo-black.png"
-import searchb from "../assets/search-b.png"
-import searchw from "../assets/search-w.png"
-import day from "../assets/day.png"
-import night from "../assets/night.png"
+import logoblack from "../assets/logo-black.png";
+import searchb from "../assets/search-b.png";
+import searchw from "../assets/search-w.png";
+import day from "../assets/day.png";
+import night from "../assets/night.png";
 
 function Navbar({ theme, SetTheme }) {
-          const toggle_mode=()=>{
-                    theme = 'light' ? SetTheme('dark') : SetTheme('light');
-          }
-  return (
-    <div className='navbar'>
-                      <img src={theme=='light'? logoblack : logowhite} className='logo' />
-                      <ul>
-                                <li>Home</li>
-                                <li>Products</li>
-                                <li>Quality</li>
-                                <li>About Us</li>
-                      </ul>
-                      <div className='search-box'>
-                                <input type='text' placeholder='search' className='input' />
-                                <img src={theme=='light'? searchb :searchw} />
-                      </div>
-                      <img onClick={()=>{toggle_mode()}} src={theme=='light'? night:day} className='toggle'/>
-    </div>
-  )
+    const toggle_mode = () => {
+        // Use === for comparison in the ternary operator
+        theme === 'light' ? SetTheme('dark') : SetTheme('light');
+    }
+
+    return (
+        <div className='navbar'>
+            {/* Use alt attribute for accessibility */}
+            <img src={theme === 'light' ? logoblack : logowhite} alt="logo" className='logo' />
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>Quality</li>
+                <li>About Us</li>
+            </ul>
+            <div className='search-box'>
+                <input type='text' placeholder='search' className='input' />
+                {/* Alt attribute for accessibility */}
+                <img src={theme === 'light' ? searchw : searchb} alt="search" />
+            </div>
+            {/* Alt attribute for accessibility */}
+            <img onClick={toggle_mode} src={theme === 'light' ? night : day} alt="toggle" className='toggle' />
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
