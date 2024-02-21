0ddd400803b0f70c125b0a47fb7a2db65099cf50
Ibrahim <ibrahim.soft.dev23@gmail.com>
2024-02-21 11:57:33 +0600

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
+  const [theme, SetTheme] = useState('light');
   return (
-    <div className="App">
-  
-    </div>
+    <div className={`container ${theme}`}>
+      <Navbar theme={theme} SetTheme={SetTheme} />
+   </div>
   );
 }
 
